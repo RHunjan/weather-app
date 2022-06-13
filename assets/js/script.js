6cc7d6ba274ae9fa38f3765d50701c70e33161b7
ReenaH <Reena_Hunjan@yahoo.com>
2022-06-12 20:29:18 -0400

    added content to daily forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@
  var cityWindEl =document.querySelector("#wind");
  var cityHumidityEl = document.querySelector("#humidity");
  var cityUvEl = document.querySelector("#uv_index");
+ var dailyForecastEl = document.querySelector(".forecast_cards");
+
+ var dailyWeather = [];
 
  
 //function to get values from data returned
@@ -13,7 +16,6 @@ var getWeather = function(){
        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ inputCityName.value + "&appid=d564b912988e6af5b9f3c1b180526f25")
     .then(function(response){
         response.json().then(function(data){
-
             // capture data
             var cityName = data['name'];
             var cityTemp = data['main']['temp'];
@@ -22,7 +24,7 @@ var getWeather = function(){
             var lat = data['coord']['lat'];
             var lon = data['coord']['lon'];
             console.log(cityName, cityTemp, cityWind, cityHumidity, lat, lon);   
-            uvIndex(lat,lon); 
+            getUvIndex(lat,lon); 
             forecast(cityName); 
             
             // display data
@@ -30,9 +32,6 @@ var getWeather = function(){
            cityTempEl.innerHTML = "Temp: " + cityTemp + " F";
            cityWindEl.innerHTML = "Wind: " + cityWind + " MPH";
            cityHumidityEl.innerHTML = "Humidity: " + cityHumidity + " %";
-
-
-
         })
         
         .catch(function(error){
@@ -42,14 +41,12 @@ var getWeather = function(){
 };
 
 // fetch to get uvindex data
-var uvIndex = function(lat,lon){
+var getUvIndex = function(lat,lon){
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" +lon +"&exclude=hourly,daily&appid=d564b912988e6af5b9f3c1b180526f25")
     .then(function(response){
         response.json().then(function(data){
             var uvIndex = data['current']['uvi'];
             cityUvEl.innerHTML = `UV Index: ${uvIndex}`;
-
-            console.log(uviIndex);
         })
     })
 };
@@ -57,6 +54,57 @@ var uvIndex = function(lat,lon){
 // fetch to get forecast data
 var forecast = function(cityName){
     fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid=d564b912988e6af5b9f3c1b180526f25")
+    .then(function(response){
+        response.json().then(function(data){
+            //capture data 
+             dailyWeather = {
+                date: data["list"][0]['dt_txt'],
+                temp: data["list"][0]['main']['temp'],
+                wind: data["list"][0]["wind"]["speed"],
+                humidity: data["list"][0]['main']['humidity']
+             }
+             //display data
+            console.log(dailyWeather);
+
+            var dateCard = document.createElement('div');
+            var displayDate = document.createElement('h4');
+            var displayTemp = document.createElement('p');
+            var displayWind = document.createElement('p');
+            var displayHumidity = document.createElement('p');
+            
+            //get the date
+            var dailyDateTime = dailyWeather.date;
+            var dailyDate = dailyDateTime.slice(0,10);
+
+            //add content to elements
+
+            displayDate.innerHTML = dailyDate;
+            displayTemp.innerHTML = `Temp: ${dailyWeather.temp} F`;
+            displayWind.innerHTML = `Wind: ${dailyWeather.wind} MPH`;
+            displayHumidity.innerHTML = `Humidity: ${dailyWeather.humidity} %`;
+
+            //append content to dateCard
+            dateCard.append(displayDate);
+            dateCard.append(displayTemp);
+            dateCard.append(displayWind);
+            dateCard.append(displayHumidity);
+          
+            console.log(dailyDate);
+
+          
+
+            dailyForecastEl.appendChild(dateCard);
+
+
+
+            //create card element
+            //create lists elements
+            //add text to list elements
+            //append list elements to card element
+            //append card element to div
+        
+        })
+    })
 };
 
 
