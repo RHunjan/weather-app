ef150b5d13fb810b9423aba597deae042514e74e
ReenaH <Reena_Hunjan@yahoo.com>
2022-06-12 22:59:33 -0400

    added daily forecasts and refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
- var buttonEl = document.querySelector(".btn");
- var inputCityName = document.querySelector(".inputName");
- var displayCityName = document.querySelector("#cityName");
- var cityTempEl = document.querySelector("#temp");
- var cityWindEl =document.querySelector("#wind");
- var cityHumidityEl = document.querySelector("#humidity");
- var cityUvEl = document.querySelector("#uv_index");
+//Declare elements
+var buttonEl = document.querySelector(".btn");
+var inputCityName = document.querySelector(".inputName");
+var displayCityName = document.querySelector("#cityName");
+var cityTempEl = document.querySelector("#temp");
+var cityWindEl =document.querySelector("#wind");
+var cityHumidityEl = document.querySelector("#humidity");
+var cityUvEl = document.querySelector("#uv_index");
  var dailyForecastEl = document.querySelector(".forecast_cards");
 
- var dailyWeather = [];
-
- 
-//function to get values from data returned
-
 var getWeather = function(){
-       fetch("https://api.openweathermap.org/data/2.5/weather?q="+ inputCityName.value + "&appid=d564b912988e6af5b9f3c1b180526f25")
+
+    cityName = inputCityName.value;
+    console.log(cityName);
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&APPID=d564b912988e6af5b9f3c1b180526f25")
     .then(function(response){
         response.json().then(function(data){
-            // capture data
+            console.log(data);
+        //capture data from fetch
             var cityName = data['name'];
             var cityTemp = data['main']['temp'];
             var cityWind = data['wind']['speed'];
@@ -25,93 +26,76 @@ var getWeather = function(){
             var lon = data['coord']['lon'];
             console.log(cityName, cityTemp, cityWind, cityHumidity, lat, lon);   
             getUvIndex(lat,lon); 
-            forecast(cityName); 
-            
-            // display data
+            //forecast(cityName); 
+
+         // display data
            displayCityName.innerHTML = cityName;
            cityTempEl.innerHTML = "Temp: " + cityTemp + " F";
            cityWindEl.innerHTML = "Wind: " + cityWind + " MPH";
            cityHumidityEl.innerHTML = "Humidity: " + cityHumidity + " %";
-        })
-        
-        .catch(function(error){
-            alert("Please enter valid city");
-        })
+       
+        }) // end of function(data)
     })
-};
+    
+
+}; // end of getWeather
 
 // fetch to get uvindex data
 var getUvIndex = function(lat,lon){
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" +lon +"&exclude=hourly,daily&appid=d564b912988e6af5b9f3c1b180526f25")
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" +lon +"&exclude=minutely,hourly&appid=d564b912988e6af5b9f3c1b180526f25")
     .then(function(response){
         response.json().then(function(data){
             var uvIndex = data['current']['uvi'];
             cityUvEl.innerHTML = `UV Index: ${uvIndex}`;
-        })
-    })
-};
 
-// fetch to get forecast data
-var forecast = function(cityName){
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid=d564b912988e6af5b9f3c1b180526f25")
-    .then(function(response){
-        response.json().then(function(data){
-            //capture data 
-             dailyWeather = {
-                date: data["list"][0]['dt_txt'],
-                temp: data["list"][0]['main']['temp'],
-                wind: data["list"][0]["wind"]["speed"],
-                humidity: data["list"][0]['main']['humidity']
-             }
-             //display data
-            console.log(dailyWeather);
+            // loop through days
+            for (var i = 0; i<5; i++){
 
-            var dateCard = document.createElement('div');
-            var displayDate = document.createElement('h4');
-            var displayTemp = document.createElement('p');
-            var displayWind = document.createElement('p');
-            var displayHumidity = document.createElement('p');
+                //capture data
+                var dailyWeather = {
+                    temp: data['daily'][i]['temp']['day'],
+                    humidity: data['daily'][i]['humidity'],
+                    windSpeed: data['daily'][i]['wind_speed']
+                };
+
+                //create elements
+                var dateCard = document.createElement('div');
+                var displayDate = document.createElement('h4');
+                var displayTemp = document.createElement('p');
+                var displayWind = document.createElement('p');
+                var displayHumidity = document.createElement('p')
+
+                 //add content to elements
+                displayDate.innerHTML = `test`
+                displayTemp.innerHTML = `Temp: ${dailyWeather.temp} F`;
+                displayWind.innerHTML = `Wind: ${dailyWeather.windSpeed} MPH`;
+                displayHumidity.innerHTML = `Humidity: ${dailyWeather.humidity} %`;
+
+                //append content to dateCard
+                 dateCard.append(displayDate);
+                 dateCard.append(displayTemp);
+                 dateCard.append(displayWind);
+                 dateCard.append(displayHumidity);
+
+                  //append dateCard to dailyForecast 
+                dailyForecastEl.appendChild(dateCard);
+
+
             
-            //get the date
-            var dailyDateTime = dailyWeather.date;
-            var dailyDate = dailyDateTime.slice(0,10);
-
-            //add content to elements
-
-            displayDate.innerHTML = dailyDate;
-            displayTemp.innerHTML = `Temp: ${dailyWeather.temp} F`;
-            displayWind.innerHTML = `Wind: ${dailyWeather.wind} MPH`;
-            displayHumidity.innerHTML = `Humidity: ${dailyWeather.humidity} %`;
-
-            //append content to dateCard
-            dateCard.append(displayDate);
-            dateCard.append(displayTemp);
-            dateCard.append(displayWind);
-            dateCard.append(displayHumidity);
-          
-            console.log(dailyDate);
-
-          
-
-            dailyForecastEl.appendChild(dateCard);
-
-
-
-            //create card element
-            //create lists elements
-            //add text to list elements
-            //append list elements to card element
-            //append card element to div
-        
+                console.log(dailyWeather);
+            
+            }
         })
     })
-};
-
-
+}; // end of uvindex
 
 
 
 buttonEl.addEventListener('click', function(){
+
  getWeather();
 
 });
+
+
+ 
